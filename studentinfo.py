026d4f3821f4e6cfe07db1c4a42ba72ea8ad44b2
Gururaj-ai-hub <gururajsutar24@gmail.com>
2026-08-14 21:04:07 +0530

    Add files via upload

---
+++ studentinfo.py
@@ -0,0 +1,20 @@
+print("------Student Information-------".center(120))
+
+name = input("Enter your Name : ")
+div = input("Enter your Division : ")
+age = int(input("Enter your Age : "))
+collegename = input("Enter your college Name : ")
+course = input("Enter your Course : ")
+prn = int(input("Enter your prn no : "))
+
+
+print("--------Student Details--------".center(120))
+
+print("Student Name : ",name)
+print("Division : ",div)
+print("Age : ",age)
+print("College Name : ",collegename)
+print("Course : ",course)
+print("PRN No : ",prn)
+
+print("*******Thank You*******")
